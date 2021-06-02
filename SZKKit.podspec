8b4211a8573bd15e34507ec4e36d4382f82f0d8e
盛子康 <>
2021-06-02 13:54:09 +0800

    操作符注释和public修饰

--- SZKKit.podspec
+++ SZKKit.podspec
@@ -15,7 +15,7 @@ Pod::Spec.new do |spec|
   #  summary should be tweet-length, and the description more in depth.
   #
 
-  spec.name         = "ZKKit"
+  spec.name         = "SZKKit"
   spec.version      = "0.0.1"
   spec.summary      = "a common framework"
 
--- Util/Operator.swift
+++ Util/Operator.swift
@@ -4,50 +4,52 @@
 //
 //  Created by 盛子康 on 2021/3/30.
 //
-
+//带log解包操作符，该操作符在测试环境上如果取nil会崩溃，正式环境回提供默认值
 infix operator !?
 
-func !?<T: ExpressibleByIntegerLiteral>
+public func !?<T: ExpressibleByIntegerLiteral>
     (wrapped: T?, failureText: @autoclosure () -> String) -> T
 {
     assert(wrapped != nil, failureText())
     return wrapped ?? 0
 }
 
-func !?<T: ExpressibleByArrayLiteral>
+public func !?<T: ExpressibleByArrayLiteral>
     (wrapped: T?, failureText: @autoclosure () -> String) -> T
 {
     assert(wrapped != nil, failureText())
     return wrapped ?? []
 }
 
-func !?<T: ExpressibleByStringLiteral>
+public func !?<T: ExpressibleByStringLiteral>
     (wrapped: T?, failureText: @autoclosure () -> String) -> T
 {
     assert(wrapped != nil, failureText())
     return wrapped ?? ""
 }
 
-func !?<T: ExpressibleByBooleanLiteral>
+public func !?<T: ExpressibleByBooleanLiteral>
     (wrapped: T?, failureText: @autoclosure () -> String) -> T
 {
     assert(wrapped != nil, failureText())
     return wrapped ?? false
 }
 
-func !?<T>(wrapped: T?,
+public func !?<T>(wrapped: T?,
               nilDefault: @autoclosure () -> (value: T, text: String)) -> T
 {
     assert(wrapped != nil, nilDefault().text)
     return wrapped ?? nilDefault().value
 }
 //some?.do("something") !? "可选链未执行该方法" 在调试终止，在正式上，该方法没执行不会闪退
-func !?(wrapped: ()?, failureText: @autoclosure () -> String) {
+public func !?(wrapped: ()?, failureText: @autoclosure () -> String) {
     assert(wrapped != nil, failureText())
 }
 
+//带log解包操作符，该操作符在测试环境正式环境取nil都会崩溃
 infix operator !!
-func !! <T>(wrapped: T?, failureText: @autoclosure () -> String) -> T {
+
+public func !! <T>(wrapped: T?, failureText: @autoclosure () -> String) -> T {
     if let x = wrapped { return x }
     fatalError(failureText())
 }
